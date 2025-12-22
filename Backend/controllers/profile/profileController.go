1d3e8718b6a8888464fc15f155bce033e71add26
Aman Koua <akoua@umich.edu>
2025-12-22 13:44:54 -0500

    stopped re-inventing the wheel

--- Backend/controllers/profile/profileController.go
+++ Backend/controllers/profile/profileController.go
@@ -4,17 +4,17 @@ import (
 	"github.com/AmanKoua/huntboard/models/profile/entity"
 	"github.com/AmanKoua/huntboard/models/profile/request"
 	"github.com/AmanKoua/huntboard/services/db"
-	"github.com/AmanKoua/huntboard/services/utils"
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 )
 
 type ProfileController struct {
 	dbService *db.Service
-	validator *utils.Validator
+	validator *validator.Validate
 }
 
 func NewController(db *db.Service) *ProfileController {
-	validate := utils.ConstructValidator()
+	validate := validator.New()
 	return &ProfileController{db, validate}
 }
 
@@ -42,7 +42,7 @@ func (this *ProfileController) createProfile(c *fiber.Ctx) error {
 
 	}
 
-	err := this.validator.Test(createProfileRequest)
+	err := this.validator.Struct(createProfileRequest)
 
 	if err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
