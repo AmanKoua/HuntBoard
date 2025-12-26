e4478161c3b0fcc8515940c6575c576e270dd726
Aman Koua <akoua@umich.edu>
2025-12-26 16:26:52 -0500

    WIP : dashboard

---
+++ Frontend/HuntBoardUi/src/components/jobListingCard/JobListingCard.tsx
@@ -0,0 +1,7 @@
+import "./JobListingCard.scss"
+
+export const JobListingCard = () => {
+
+    return <div className="listing-card"> job listing content here! </div>
+
+}
--- Frontend/HuntBoardUi/src/pages/dashboard/Dashboard.tsx
+++ Frontend/HuntBoardUi/src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,17 @@
+import { JobListingCard } from "../../components/jobListingCard/JobListingCard"
+import "./Dashboard.scss"
+
 export const Dashboard = () => {
 
     return <main>
-        <div>Testing123</div>
+        <section className="listings-section">
+            <h2>Job Listings</h2>
+            <div className="listings-section__drawer">
+                <JobListingCard/>
+                <JobListingCard/>
+                <JobListingCard/>
+            </div>
+        </section>
     </main>
 
 }
