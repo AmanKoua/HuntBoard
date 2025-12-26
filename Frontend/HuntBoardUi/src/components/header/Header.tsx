523519b07849ab33662c3a973b6c690de8d8f120
Aman Koua <akoua@umich.edu>
2025-12-26 17:52:30 -0500

    navigate on profile button click

--- Frontend/HuntBoardUi/src/components/header/Header.tsx
+++ Frontend/HuntBoardUi/src/components/header/Header.tsx
@@ -1,11 +1,17 @@
 import "./header.scss"
+import { useNavigate } from "react-router";
 
 export const Header = () => {
+
+    const navigate = useNavigate();
+
     return <header>
         <p className="title">
             HuntBoard
         </p>
-        <button>
+        <button onClick={()=>{
+            navigate("/")
+        }}>
             Profile
         </button>
     </header>
