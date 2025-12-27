81cc2b614e5c741646a43d8ffb7ea6226e9b6671
Aman Koua <akoua@umich.edu>
2025-12-27 12:04:41 -0500

    updated vite config, and added job status row

--- Frontend/HuntBoardUi/src/components/createJobListingContent/CreateJobListingContent.tsx
+++ Frontend/HuntBoardUi/src/components/createJobListingContent/CreateJobListingContent.tsx
@@ -1,15 +1,17 @@
 import { SelectorRow } from "../selectorRow/SelectorRow"
 import { useState } from "react"
 import "./CreateJobListingContent.scss"
-import { jobLevelDict, jobLocationDict } from "../../utils/types"
+import { jobLevelDict, jobLocationDict, jobStatusDict } from "../../utils/types"
 
 export const CreateJobListingContent = () => {
 
-    const jobStatusOptions = Object.keys(jobLocationDict)
+    const jobLocationOptions = Object.keys(jobLocationDict)
+    const jobStatusOptions = Object.keys(jobStatusDict)
     const jobLevelOptions = Object.keys(jobLevelDict)
 
     const [location, setLocation] = useState("")
     const [jobLevel, setJobLevel] = useState("")
+    const [status, setStatus] = useState("")
 
     return <div className='modal-content'>
         <div className='modal-content__input-row'>
@@ -23,7 +25,7 @@ export const CreateJobListingContent = () => {
             <p>
                <strong>Location : </strong> 
             </p>
-            <SelectorRow value={location} setValue={setLocation} options={jobStatusOptions}/>
+            <SelectorRow value={location} setValue={setLocation} options={jobLocationOptions}/>
         </div>
 
         <div className='modal-content__input-row'>
@@ -66,6 +68,14 @@ export const CreateJobListingContent = () => {
                <strong>Salary : </strong> 
             </p>
             <input placeholder="100,000"/>
+        </div> 
+
+
+        <div className='modal-content__input-row'>
+            <p>
+               <strong>Status : </strong> 
+            </p>
+            <SelectorRow value={status} setValue={setStatus} options={jobStatusOptions}/>
         </div> 
 
         <button className='modal-content__button'>Create Job Listing</button>
